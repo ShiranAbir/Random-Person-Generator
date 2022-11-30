1817f28e9970c64f1b0230addc5910e0091e46c3
ShiranAbir <106183696+ShiranAbir@users.noreply.github.com>
2022-11-30 15:07:59 +0200

    Fix pages bug #3

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Random-Person-Generator/',
   plugins: [vue()],
   resolve: {
     alias: {
